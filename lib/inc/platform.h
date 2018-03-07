bff23e2e37d238bdea67efc74d137b9fd3d15162
Lukas Zronek <lukas-zronek@users.noreply.github.com>
2018-03-07 09:16:27 +0100

    Make sure that the header file is included only once

--- lib/inc/platform.h
+++ lib/inc/platform.h
@@ -1,3 +1,6 @@
+#ifndef _PLATFORM_H_
+#define _PLATFORM_H_
+
 #ifdef __APPLE__
 /* .type directive is not available on macOS, it is overwritten with an empty macro */
 .macro .type
@@ -7,3 +10,5 @@
 .section __TEXT,__const
 .endm
 #endif
+
+#endif /* _PLATFORM_H_ */
