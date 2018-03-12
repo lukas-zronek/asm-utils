3d2272720ac00c2d86cb06fe3472e45fa6eec8c6
Lukas Zronek <lukas-zronek@users.noreply.github.com>
2018-03-12 15:31:12 +0100

    ls: Implement opendir() and readdir() instead of directly using system calls. This allows to use different syscalls to list a directory.

---
+++ lib/inc/dir.h
@@ -0,0 +1,11 @@
+#ifndef _DIR_H_
+#define _DIR_H_
+
+#define DIR_BUFFER_SIZE 1024
+
+/* offsets of dir structure used by opendir and readdir */
+#define DIR_FD 0
+#define DIR_BUFFER 4
+#define DIR_OFFSET 8
+
+#endif
