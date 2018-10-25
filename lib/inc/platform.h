3b50c9e93694549ce6fb4f7e87d928257ca9a1f8
Lukas Zronek <lukas-zronek@users.noreply.github.com>
2018-10-25 19:27:29 +0200

    preliminary support of OpenBSD

--- lib/inc/platform.h
+++ lib/inc/platform.h
@@ -14,5 +14,18 @@
 .section .rodata
 .endm
 #endif
+#if defined(__OpenBSD__)
+/*
+ * From https://cvsweb.openbsd.org/cgi-bin/cvsweb/~checkout~/src/lib/csu/os-note-elf.h?rev=1.5&content-type=text/plain
+ */
+.section ".note.openbsd.ident", "a"
+	.p2align 2
+	.long   8
+	.long   4
+	.long   1
+	.asciz "OpenBSD"
+	.long   0
+	.previous
+#endif
 
 #endif /* _PLATFORM_H_ */
