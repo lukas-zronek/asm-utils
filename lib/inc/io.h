a9b6c3a5cc187afed44ae09f945d5d634884f142
Lukas Zronek <lukas-zronek@users.noreply.github.com>
2018-11-14 22:09:35 +0100

    Linux: Fix cp
    The flags for the syscall open have different values on Linux.

--- lib/inc/io.h
+++ lib/inc/io.h
@@ -6,8 +6,15 @@
 #define STDERR_FILENO	2
 #define O_RDONLY	0x0000
 #define O_WRONLY	0x0001
+
+#ifdef __linux__
+#define O_CREAT		0x0080
+#define O_EXCL		0x0040
+#define O_DIRECTORY	0x10000
+#else
 #define O_CREAT		0x0200
 #define O_EXCL		0x0800
 #define O_DIRECTORY	0x100000
+#endif /* __linux__*/
 
-#endif
+#endif /* _IO_H_ */
