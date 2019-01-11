851aa76f7d3c0167bc31537caa5596eb33f35f5a
Lukas Zronek <lukas-zronek@users.noreply.github.com>
2019-01-11 08:11:05 +0100

    Add the d prefix to the functions that use a file descriptor as an argument and remove the f prefix

--- test/arg.c
+++ test/arg.c
@@ -1,18 +1,18 @@
 int main(int argc, char *argv[], char *envp[]);
-int fprintf(int fd, const char * restrict format, ...);
+int dprintf(int fd, const char * restrict format, ...);
 
 int main(int argc, char *argv[], char *envp[])
 {
-	fprintf(1, "argc: %d\n", argc);
+	dprintf(1, "argc: %d\n", argc);
 
 	for (int i = 0; i < argc; i++) {
-		fprintf(1, "argv[%d]: %s\n", i, argv[i]);
+		dprintf(1, "argv[%d]: %s\n", i, argv[i]);
 	}
 
-	fprintf(1, "env:\n");
+	dprintf(1, "env:\n");
 
 	while(*envp)
-		fprintf(1, "%s\n",*envp++);
+		dprintf(1, "%s\n",*envp++);
 
 	return 0;
 }
