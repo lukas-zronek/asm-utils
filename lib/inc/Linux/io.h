45e69a565ed1a116bdc6a34964189dc9049d0150
Lukas Zronek <lukas-zronek@users.noreply.github.com>
2021-03-21 12:02:11 +0100

    Move platform-specific include files to a separate directory

---
+++ lib/inc/Linux/io.h
@@ -0,0 +1,14 @@
+#ifndef _IO_H_
+#define _IO_H_
+
+#define STDIN_FILENO    0
+#define STDOUT_FILENO   1
+#define STDERR_FILENO   2
+
+#define O_RDONLY	0x0000
+#define O_WRONLY	0x0001
+#define O_CREAT		0x0080
+#define O_EXCL		0x0040
+#define O_DIRECTORY	0x10000
+
+#endif /* _IO_H_ */
--- lib/inc/io.h
+++ lib/inc/io.h
@@ -4,17 +4,11 @@
 #define STDIN_FILENO	0
 #define STDOUT_FILENO	1
 #define STDERR_FILENO	2
+
 #define O_RDONLY	0x0000
 #define O_WRONLY	0x0001
-
-#ifdef __linux__
-#define O_CREAT		0x0080
-#define O_EXCL		0x0040
-#define O_DIRECTORY	0x10000
-#else
 #define O_CREAT		0x0200
 #define O_EXCL		0x0800
 #define O_DIRECTORY	0x100000
-#endif /* __linux__*/
 
 #endif /* _IO_H_ */
